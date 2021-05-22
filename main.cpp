8c8f3a9c38323228840fa0cc982674ed54e8438b
Elias Daniel Bromerschenkel <>
2021-05-22 18:41:58 +0000

    declaracoes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
-#include <iostream>
+include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
-}
+using namespace std;
+#define MAX_SIZE 255
+char msg[MAX_SIZE];
+char nova_msg[MAX_SIZE];
+
+int encode(char *mensagem, char from_char, char to_char, char *nova_msg);
